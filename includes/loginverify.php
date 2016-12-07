89806eb9f3e44263ed25cf5350f03012d7feda7f
estherleah <estherleahmorrison@gmail.com>
2016-12-07 19:06:23 +0000

    Update loginverify.php

--- includes/loginverify.php
+++ includes/loginverify.php
@@ -6,7 +6,7 @@ include '../db/dbh.php';
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-$sql = "SELECT * FROM users WHERE uid = '$uid' AND pwd = '$pwd'";
+$sql = "SELECT * FROM dbo.users WHERE uid = '$uid' AND pwd = '$pwd'";
 $result = mysqli_query($conn, $sql);
 
 if (!$row = mysqli_fetch_assoc($result)){
