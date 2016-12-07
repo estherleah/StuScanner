c320de122bc3716a15c3640b3f639dcf8686518e
estherleah <estherleahmorrison@gmail.com>
2016-12-07 19:52:54 +0000

    db password

--- db/dbh.php
+++ db/dbh.php
@@ -2,7 +2,7 @@
 
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:gc0216.database.windows.net,1433; Database = OCR", "willredmondson", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:gc0216.database.windows.net,1433; Database = OCR", "willredmondson", "Harvey1Esther2Will3");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,7 +11,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "willredmondson@gc0216", "pwd" => "{your_password_here}", "Database" => "OCR", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "willredmondson@gc0216", "pwd" => "Harvey1Esther2Will3", "Database" => "OCR", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:gc0216.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
