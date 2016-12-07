eebb941b069ad117d5406d88bf3ce5aef3ec87c3
estherleah <estherleahmorrison@gmail.com>
2016-12-07 18:50:30 +0000

    db connection

--- db/dbh.php
+++ db/dbh.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("tcp:gc0216.database.windows.net,1433", "willredmondson@gc0216", "Harvey1Esther2Will3", "OCR");
+$conn = mysqli_connect("gc0216.database.windows.net,1433", "willredmondson@gc0216", "Harvey1Esther2Will3", "OCR");
 
 if (!$conn){
     die("Connection failed: ".mysqli_connect_error());
