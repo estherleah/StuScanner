326310ae7f5153e55714d8d5587e830cac3f06b0
estherleah <estherleahmorrison@gmail.com>
2016-12-07 19:41:37 +0000

    db

--- db/dbh.php
+++ db/dbh.php
@@ -1,14 +1,8 @@
 <?php
 
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:gc0216.database.windows.net,1433; Database = OCR", "willredmondson", "Harvey1Esther2Will3");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+
+
+$conn = mysqli_connect("tcp:gc0216.database.windows.net,1433", "willredmondson", "Harvey1Esther2Will3", "OCR");
 
 ?>
 
