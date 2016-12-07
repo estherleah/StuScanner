b20e78d6c8c56150acdcfb965ec2a0b058e2fd32
estherleah <estherleahmorrison@gmail.com>
2016-12-07 21:37:13 +0000

    test

--- db/dbh.php
+++ db/dbh.php
@@ -1,8 +1,8 @@
 <?php
 
-$connectstr_dbhost = 'gc0216.database.windows.net';
+$connectstr_dbhost = 'g16.database.windows.net';
 $connectstr_dbname = 'localdb';
-$connectstr_dbusername = 'willredmondson';
+$connectstr_dbusername = 'test';
 $connectstr_dbpassword = 'Harvey1Esther2Will3';
 
 foreach ($_SERVER as $key => $value) {
@@ -17,5 +17,17 @@ foreach ($_SERVER as $key => $value) {
 }
 
 $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+mysqli_close($conn);
 
 ?>
