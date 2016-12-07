b8067bdf88396256007d7101abecd9707da6dd88
estherleah <estherleahmorrison@gmail.com>
2016-12-07 20:54:31 +0000

    test

--- db/dbh.php
+++ db/dbh.php
@@ -28,7 +28,7 @@ if (!$link) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-mysqli_close($link);
+
 
 ?>
 
