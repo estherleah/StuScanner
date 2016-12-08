bfe8a0438ee1df7cdeed3c293f3767372aaf149c
estherleah <estherleahmorrison@gmail.com>
2016-12-08 13:13:14 +0000

    Update data.php

--- data.php
+++ data.php
@@ -6,17 +6,7 @@
 //header('Content-Type: application/json');
 
 //database
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'quexf');
-
-//get connection
-$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if(!$mysqli){
-	die("Connection failed: " . $mysqli->error);
-}
+include '../db/dbh.php';
 
 //query to get data from the table
 
