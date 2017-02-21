4fff62cc8daffc42eb43307a551b0fea16364f13
estherleah <estherleahmorrison@gmail.com>
2017-02-21 12:17:33 +0000

    Bug fix for Linux

--- quexf-1.18.1/identifier.php
+++ quexf-1.18.1/identifier.php
@@ -3,6 +3,6 @@ $user_agent = getenv("HTTP_USER_AGENT");
 
 if(strpos($user_agent, "Win") !== FALSE)
 $os = "Windows";
-elseif(strpos($user_agent, "Mac") !== FALSE)
-$os = "Mac";
+else
+$os = "Linux";
 ?>
